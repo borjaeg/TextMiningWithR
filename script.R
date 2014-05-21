65cc534eb4f32fbf16e54895991150298894eba4
Borja A. Espejo-Garcia <borja3790@gmail.com>
2014-05-22 01:11:08 +0200

    First part of the script commited

---
+++ script.R
@@ -0,0 +1,8 @@
+cname <- file.path(".") # Assuming this directory stores your files ;-)
+cname
+
+dir(cname) # list of files
+length(dir(cname)) # list length
+
+library(tm) # if you don't have it -> install.packages("tm")
+
